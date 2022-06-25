e7a218cc7545feee46ba85fd6be5c97e98cb83f9
LiuHeng <1198008940@qq.com>
2022-06-25 20:30:34 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,7 +7,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
-
+        System.out.println("hello git2!");
     }
-
-}
+ }
