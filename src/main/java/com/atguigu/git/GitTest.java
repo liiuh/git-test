4a7af58c8c014fcda3330c0502d1c77fcd5e1679
liiuh <33833752+liiuh@users.noreply.github.com>
2022-06-26 10:22:14 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hotfix test");
         System.out.println("push test");
         System.out.println("push test2");
+        System.out.println("pull test");
     }
  }
